0c398f67033d22bd7484a913661de4b7e050fb82
Brian Angulo <brianangulo96@gmail.com>
2021-05-29 16:56:31 -0400

    trying to get material icons to work but no success

--- src/views/Navigators.js
+++ src/views/Navigators.js
@@ -1,12 +1,12 @@
 //Core imports
 import React from 'react';
-import { useWindowDimensions } from "react-native";
+import {useWindowDimensions} from 'react-native';
 //Dependecies imports
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 //Views Imports
 import WelcomeView from './WelcomeView';
-import Feed from "./FeedView";
+import Feed from './FeedView';
 
 //Caching navigators + any hooks + extras
 const Stack = createStackNavigator();
@@ -21,24 +21,26 @@ const WelcomeViewStack = () => {
   );
 };
 const FeedStack = () => {
-  <Stack.Navigator>
-    <Stack.Screen name="Yo!" component={Feed} />
-  </Stack.Navigator>
-}
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Yo!" component={Feed} />
+    </Stack.Navigator>
+  );
+};
 //Main drawer navigator
 const MainDrawerNav = () => {
-    const dimensions = useWindowDimensions();
-    const isLargeScreen = dimensions.width >= 768;
-    return (
-      <Drawer.Navigator
-        drawerType="permanent"
-        drawerStyle={{
-          width: isLargeScreen ? '17%' : '20%',
-        }}>
-        <Drawer.Screen name="Welcome" component={WelcomeViewStack} />
-        <Drawer.Screen name="Feed" component={FeedStack} />
-      </Drawer.Navigator>
-    );
-}
+  const dimensions = useWindowDimensions();
+  const isLargeScreen = dimensions.width >= 768;
+  return (
+    <Drawer.Navigator
+      drawerType="permanent"
+      drawerStyle={{
+        width: isLargeScreen ? '17%' : '20%',
+      }}>
+      <Drawer.Screen name="Welcome" component={WelcomeViewStack} />
+      <Drawer.Screen name="Feed" component={FeedStack} />
+    </Drawer.Navigator>
+  );
+};
 
 export default MainDrawerNav;
