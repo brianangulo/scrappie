6a886f53ba798f8be29f5d69841d1b029c41725b
Brian Angulo <brianangulo96@gmail.com>
2021-05-29 17:48:43 -0400

    Base newsfeed scrolling view created and font awesome added

--- src/views/FeedView.js
+++ src/views/FeedView.js
@@ -1,33 +1,83 @@
 //Core imports
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, ScrollView, View, List} from 'react-native';
 //Dependencies imports
-import {Card, Button, Text } from 'react-native-elements';
+import {Card, Button, Text, ListItem} from 'react-native-elements';
 //Assets imports
 import cardImg from '../assets/img/splash.jpg';
 
+
 function Feed() {
+
+  const arr = [
+    {
+      news: 'News1',
+      view: 'ViewNews1',
+    },
+    {
+      news: 'News2',
+      view: 'ViewNews2',
+    },
+    {
+      news: 'News3',
+      view: 'ViewNews3',
+    },
+    {
+      news: 'News4',
+      view: 'ViewNews4',
+    },
+    {
+      news: 'News5',
+      view: 'ViewNews5',
+    },
+    {
+      news: 'News6',
+      view: 'ViewNews6',
+    },
+    {
+      news: 'News7',
+      view: 'ViewNews7',
+    },
+    {
+      news: 'News7',
+      view: 'ViewNews7',
+    },
+  ];
+
+  const NewsList = () => {
+    return (
+    arr.map((obj, index) => {
+      return (
+          <Card key={index}>
+            <Card.Title>{obj.news}</Card.Title>
+            <Card.Divider />
+            <Card.Image source={cardImg}>
+              <Text style={{marginBottom: 10}}>
+                The idea with React Native Elements is more about component
+                structure than actual design.
+              </Text>
+            </Card.Image>
+            <Button
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0,
+              }}
+              title={obj.view}
+            />
+          </Card>
+      );
+    })
+    );
+
+  }
+
   return (
-    <Card>
-      <Card.Title>News!</Card.Title>
-      <Card.Divider />
-      <Card.Image source={require('../assets/img/splash.jpg')}>
-        <Text style={{marginBottom: 10}}>
-          The idea with React Native Elements is more about component structure
-          than actual design.
-        </Text>
-        <Button
-          buttonStyle={{
-            borderRadius: 0,
-            marginLeft: 0,
-            marginRight: 0,
-            marginBottom: 0,
-          }}
-          title="VIEW NOW"
-        />
-      </Card.Image>
-    </Card>
-  );
+      <ScrollView>
+        <NewsList />
+      </ScrollView>
+    );
 }
 
 // const styles = StyleSheet.create({
