3f230a8c872ad3ec43c0b3859a832e90c6246657
Brian Angulo <brianangulo96@gmail.com>
2021-05-30 01:23:44 -0400

    Saving bookmarks to the state

--- src/views/BookmarksView.js
+++ src/views/BookmarksView.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { ScrollView, View } from 'react-native';
-import { Text, Card } from "react-native-elements";
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native';
+import {useSelector} from 'react-redux';
+import FeedList from '../components/FeedComponent';
 
 function Bookmarks() {
+    const savedUrls = useSelector((state) => state.root.savedUrls);
+    
     return (
-        <Card>
-            <Card.Title>News1</Card.Title>
-            <Card.Divider />
-            <Card.Image source={require("../assets/img/splash.jpg")} />
-            <Text>These are some of the news you have saved!</Text>
-        </Card>
+      <SafeAreaView>
+        <FeedList url={savedUrls} />
+      </SafeAreaView>
     );
 }
 
